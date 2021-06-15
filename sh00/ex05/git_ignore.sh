8a595b14fa8c079e5e6762129153a26f5b5d2df7
joalbert <joalbert@student.42sp.org.br>
2021-06-15 17:27:06 +0000

    ex05

--- sh00/ex05/git_ignore.sh
+++ sh00/ex05/git_ignore.sh
@@ -1,4 +1,1 @@
-git_root_directory=$(git rev-parse --show-toplevel)
-cd $git_root_directory
-directory_files=$(find . -type f)
-git check-ignore $directory_files
+git ls-files -o -i --exclude-standard
