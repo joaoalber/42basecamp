6552836b1040af34b11a641abf5fb37591152b13
joalbert <joalbert@student.42sp.org.br>
2021-06-16 17:28:10 +0000

    modifies variable name and treat unsigned chars

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -1,14 +1,16 @@
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	int	byte;
+	unsigned int	compared_byte;
 
-	byte = 1;
+	compared_byte = 1;
+	if (n == 0)
+		return (0);
 	while (*s1 && *s2)
 	{
-		if ((*s1 != *s2) || (byte++ == n))
+		if ((*s1 != *s2) || (compared_byte++ == n))
 			break ;
 		s1++;
 		s2++;
 	}
-	return (*s1 - *s2);
+	return ((unsigned char) *s1 - (unsigned char) *s2);
 }
